6097b0b143f32156336e061c7ddf2c49f1c73f32
TinyViolette <tinyvioletter+github@gmail.com>
2026-06-14 11:10:31 +0800

    open flutter map at 0,0

---
+++ paperplane/lib/constants/map_constants.dart
@@ -0,0 +1,6 @@
+import 'package:latlong2/latlong.dart';
+
+class MapConstants {
+  static const double initialZoom = 6;
+  static const LatLng initialCenter = LatLng(0, 0);
+}
---
+++ paperplane/lib/pages/map_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:paperplane/constants/map_constants.dart';
+
+class MapPage extends StatelessWidget {
+  const MapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: MapConstants.initialCenter,
+          initialZoom: MapConstants.initialZoom,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.paperplane',
+          ),
+        ],
+      ),
+    );
+  }
+}
