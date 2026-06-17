c6f4540470ad2058400d2dd281d3ca0f34f0ecf0
TinyViolette <tinyvioletter+github@gmail.com>
2026-06-17 16:59:46 +0800

    refactor `FunctionSwitcher` for animated transitions and state-based control
    Updates the UI to use `AnimatedSwitcher` for a more compact and animated button. Introduces logic to disable the 'mark location' function when the plane is in flight, preventing invalid actions.

--- paperplane/lib/widgets/function_switcher.dart
+++ paperplane/lib/widgets/function_switcher.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:paperplane/cubit/function_switcher/function_switcher_cubit.dart';
 import 'package:paperplane/cubit/function_switcher/function_switcher_state.dart';
+import 'package:paperplane/cubit/plane/plane_state.dart';
 
-class FunctionSwitcher extends StatelessWidget {
+class FunctionSwitcher extends StatefulWidget {
   final VoidCallback onSwipeLeft;
   final VoidCallback onSwipeRight;
   final VoidCallback onTap;
@@ -15,54 +16,99 @@ class FunctionSwitcher extends StatelessWidget {
     required this.onTap,
   });
 
+  @override
+  State<FunctionSwitcher> createState() => _FunctionSwitcherState();
+}
+
+class _FunctionSwitcherState extends State<FunctionSwitcher> {
   static const List<IconData> _icons = [
     Icons.add_location_alt,
     Icons.casino,
   ];
+
+  int _slideDirection = 0;
+
+  void _onSwipeLeft() {
+    setState(() => _slideDirection = -1);
+    widget.onSwipeLeft();
+  }
+
+  void _onSwipeRight() {
+    setState(() => _slideDirection = 1);
+    widget.onSwipeRight();
+  }
+
+  static const double _disabledAlpha = 0.5;
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
       right: 16,
       top: 16,
-      child: BlocBuilder<FunctionSwitcherCubit, FunctionSwitcherState>(
-        builder: (context, state) {
-          return GestureDetector(
-            onHorizontalDragEnd: (details) {
-              if (details.primaryVelocity != null) {
-                if (details.primaryVelocity! < 0) {
-                  onSwipeLeft();
-                } else if (details.primaryVelocity! > 0) {
-                  onSwipeRight();
-                }
-              }
-            },
-            onTap: onTap,
-            child: Container(
-              width: 120,
-              height: 60,
-              decoration: BoxDecoration(
-                color: Colors.black54,
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(_icons.length, (index) {
-                  final isSelected = state.selectedIndex == index;
-                  return AnimatedContainer(
-                    duration: const Duration(milliseconds: 300),
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Icon(
-                      _icons[index],
-                      size: isSelected ? 36 : 24,
-                      color: isSelected
-                          ? Colors.white
-                          : Colors.white.withValues(alpha: 0.5),
+      child: BlocBuilder<Cubit<PlaneState>, PlaneState>(
+        builder: (context, planeState) {
+          return BlocBuilder<FunctionSwitcherCubit, FunctionSwitcherState>(
+            builder: (context, state) {
+              final isMarkEnabled =
+                  state.selectedIndex != 0 || planeState.isLanded;
+              return GestureDetector(
+                onHorizontalDragEnd: (details) {
+                  if (details.primaryVelocity != null) {
+                    if (details.primaryVelocity! < 0) {
+                      _onSwipeLeft();
+                    } else if (details.primaryVelocity! > 0) {
+                      _onSwipeRight();
+                    }
+                  }
+                },
+                onTap: isMarkEnabled ? widget.onTap : null,
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 300),
+                      switchInCurve: Curves.easeInOut,
+                      switchOutCurve: Curves.easeInOut,
+                      transitionBuilder: (child, animation) {
+                        final offset = _slideDirection >= 0
+                            ? const Offset(1.0, 0.0)
+                            : const Offset(-1.0, 0.0);
+                        return SlideTransition(
+                          position: Tween<Offset>(
+                            begin: offset,
+                            end: Offset.zero,
+                          ).animate(animation),
+                          child: child,
+                        );
+                      },
+                      layoutBuilder: (currentChild, previousChildren) {
+                        return Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            ...previousChildren,
+                            ?currentChild,
+                          ],
+                        );
+                      },
+                      child: Icon(
+                        _icons[state.selectedIndex],
+                        key: ValueKey<int>(state.selectedIndex),
+                        size: 20,
+                        color: isMarkEnabled
+                            ? Colors.white
+                            : Colors.white.withValues(alpha: _disabledAlpha),
+                      ),
                     ),
-                  );
-                }),
-              ),
-            ),
+                  ),
+                ),
+              );
+            },
           );
         },
       ),
